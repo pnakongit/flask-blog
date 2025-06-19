34376699c8341384d4ec46c5bddd27466f9b9d9c
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2025-06-19 17:33:46 +0300

    Add self-referential followers relationship to User model

--- app/models.py
+++ app/models.py
@@ -8,6 +8,15 @@ from flask_login import UserMixin
 
 from app import db
 
+followers = sa.Table(
+    'followers',
+    db.metadata,
+    sa.Column('follower_id', sa.Integer, sa.ForeignKey('user.id'),
+              primary_key=True),
+    sa.Column('followed_id', sa.Integer, sa.ForeignKey('user.id'),
+              primary_key=True)
+)
+
 
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -20,6 +29,18 @@ class User(UserMixin, db.Model):
     )
 
     posts: so.WriteOnlyMapped["Post"] = so.relationship(back_populates="author")
+    following: so.WriteOnlyMapped["User"] = so.relationship(
+        secondary="followers",
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        back_populates="followers"
+    )
+    followers: so.WriteOnlyMapped["User"] = so.relationship(
+        secondary="followers",
+        primaryjoin=(followers.c.followed_id == id),
+        secondaryjoin=(followers.c.follower_id == id),
+        back_populates="following"
+    )
 
     def __repr__(self) -> str:
         return f"<User {self.username}>"
