1f4bb8f500a5151d1a467de7f2229d2307715f98
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2025-06-23 16:00:28 +0300

    Add tests module with User test case

---
+++ tests.py
@@ -0,0 +1,114 @@
+import os
+
+os.environ["SQLALCHEMY_DATABASE_URI"] = "sqlite://"  # NOQA
+
+from datetime import datetime, timezone, timedelta
+import unittest
+from werkzeug.security import generate_password_hash
+from app import app, db
+from app.models import User, Post
+
+
+class UserModelCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.app_context = app.app_context()
+        self.app_context.push()
+        print(db)
+        db.create_all()
+
+    def tearDown(self) -> None:
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_string_representation(self) -> None:
+        user = User(username="test_username", email="test_email@example.com")
+        self.assertEqual(str(user), f"<User {user.username}>")
+
+    def test_password_hashing(self) -> None:
+        password = "my_password"
+        user = User(username="test_username", email="test_email@example.com")
+        user.set_password(password)
+
+        self.assertTrue(user.password_hash, generate_password_hash(password))
+
+    def test_password_checking(self) -> None:
+        password = "my_password"
+        not_password = "another_password"
+        user = User(username="test_username", email="test_email@example.com")
+        user.set_password(password)
+
+        self.assertTrue(user.check_password(password))
+        self.assertFalse(user.check_password(not_password))
+
+    def test_avatar(self) -> None:
+        expected_link = ("https://www.gravatar.com/avatar/"
+                         "2b9f1a4eed1215f6de2e2f80e17a27c0?d=identicon&s=128")
+        user = User(username="test", email="test_avatar@example.com")
+
+        self.assertEqual(user.avatar(128), expected_link)
+
+    def test_follow(self) -> None:
+        u1 = User(username='john', email='john@example.com')
+        u2 = User(username='susan', email='susan@example.com')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        following = db.session.scalars(u1.following.select()).all()
+        followers = db.session.scalars(u2.followers.select()).all()
+        self.assertEqual(following, [])
+        self.assertEqual(followers, [])
+
+        u1.follow(u2)
+        db.session.commit()
+        self.assertTrue(u1.is_following(u2))
+        self.assertEqual(u1.following_count(), 1)
+        self.assertEqual(u2.followers_count(), 1)
+        u1_following = db.session.scalars(u1.following.select()).all()
+        u2_followers = db.session.scalars(u2.followers.select()).all()
+        self.assertEqual(u1_following[0].username, 'susan')
+        self.assertEqual(u2_followers[0].username, 'john')
+
+        u1.unfollow(u2)
+        db.session.commit()
+        self.assertFalse(u1.is_following(u2))
+        self.assertEqual(u1.following_count(), 0)
+        self.assertEqual(u2.followers_count(), 0)
+
+    def test_follow_posts(self) -> None:
+        u1 = User(username='john', email='john@example.com')
+        u2 = User(username='susan', email='susan@example.com')
+        u3 = User(username='mary', email='mary@example.com')
+        u4 = User(username='david', email='david@example.com')
+        db.session.add_all([u1, u2, u3, u4])
+
+        now = datetime.now(timezone.utc)
+        p1 = Post(body="post from john", author=u1,
+                  timestamp=now + timedelta(seconds=1))
+        p2 = Post(body="post from susan", author=u2,
+                  timestamp=now + timedelta(seconds=4))
+        p3 = Post(body="post from mary", author=u3,
+                  timestamp=now + timedelta(seconds=3))
+        p4 = Post(body="post from david", author=u4,
+                  timestamp=now + timedelta(seconds=2))
+        db.session.add_all([p1, p2, p3, p4])
+        db.session.commit()
+
+        u1.follow(u2)
+        u1.follow(u4)
+        u2.follow(u3)
+        u3.follow(u4)
+        db.session.commit()
+
+        f1 = db.session.scalars(u1.following_posts()).all()
+        f2 = db.session.scalars(u2.following_posts()).all()
+        f3 = db.session.scalars(u3.following_posts()).all()
+        f4 = db.session.scalars(u4.following_posts()).all()
+        self.assertEqual(f1, [p2, p4, p1])
+        self.assertEqual(f2, [p2, p3])
+        self.assertEqual(f3, [p3, p4])
+        self.assertEqual(f4, [p4])
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
