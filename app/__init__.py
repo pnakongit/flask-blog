ba8b0ae3f71fc2a4096a9020042a8174f65b4fa1
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2025-07-02 19:06:49 +0300

    Set defaul login message & init Bable

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 from flask_migrate import Migrate
 
 from app.db import db
-from app.extensions import login, mail, moment
+from app.extensions import login, mail, moment, babel, get_locale
 from app.logging_setup import setup_logging
 
 app = Flask(__name__)
@@ -13,10 +13,13 @@ migrate = Migrate(app, db)
 
 login.init_app(app)
 login.login_view = app.config["LOGIN_VIEW"]
+login.login_message = app.config["LOGIN_MESSAGE"]
 
 mail.init_app(app)
 
 moment.init_app(app)
+
+babel.init_app(app, locale_selector=get_locale)
 
 setup_logging(app)
 
