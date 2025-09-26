6a5aa6ebbe51fa14b5aac9824b11b51357f7f645
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2025-09-26 21:39:38 +0300

    Create SearchForm in main.forms module

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 import sqlalchemy as sa
+from flask import request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length, ValidationError
@@ -34,3 +35,14 @@ class EmptySubmitForm(FlaskForm):
 class PostForm(FlaskForm):
     post = TextAreaField(_l("Say something"), validators=[DataRequired(), Length(min=1, max=140)])
     submit = SubmitField(_l("Submit"))
+
+
+class SearchForm(FlaskForm):
+    q = StringField(_l("Search"), validators=[DataRequired()])
+
+    def __init__(self, *args, **kwargs) -> None:
+        if "formdata" not in kwargs:
+            kwargs["formdata"] = request.args
+        if "meta" not in kwargs:
+            kwargs["meta"] = {"csrf": False}
+        super().__init__(*args, **kwargs)
